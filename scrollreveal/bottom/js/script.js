53eb4f874e13a44533fc55b3d2962d2b335dfae7
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-11-30 22:01:43 +0900

    add scroll reveal for bottom.

---
+++ scrollreveal/bottom/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    ScrollReveal().reveal('.image', {
+        delay: 300,         // fadeInアニメーションを開始するまでの時間(ms)
+        distance: '120px',  // スライドする要素の移動距離
+        duration: 1000,     // fadeInアニメーションの時間(ms)
+        origin: 'bottom',      // どの方向からスライドするか
+        reset: false        // fadeInアニメーションをスクロールする度に発火させるかどうか
+    });
+});
