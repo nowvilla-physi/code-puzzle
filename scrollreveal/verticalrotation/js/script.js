477a1059d5a1c9f2dab2200a6ba4555cf174099e
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-11-30 22:11:49 +0900

    add scroll reveal for vertical rotation.

---
+++ scrollreveal/verticalrotation/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    ScrollReveal().reveal('.image', {
+        delay: 300,      // fadeInアニメーションを開始するまでの時間(ms)
+        duration: 1000,  // fadeInアニメーションの時間(ms)
+        reset: false,    // fadeInアニメーションをスクロールする度に発火させるかどうか
+        rotate: {        // 回転の設定(3次元で指定可能)
+            x: 180,
+            y: 0,
+            z: 0
+        },
+    });
+});
