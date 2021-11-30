ecd1714afc8436da9b188e6beec122a8f7b7ddf5
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-11-30 22:07:48 +0900

    add scroll reveal for right.

---
+++ scrollreveal/right/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    ScrollReveal().reveal('.image', {
+        delay: 300,         // fadeInアニメーションを開始するまでの時間(ms)
+        distance: '400px',  // スライドする要素の移動距離
+        duration: 1000,     // fadeInアニメーションの時間(ms)
+        origin: 'right',    // どの方向からスライドするか
+        reset: false        // fadeInアニメーションをスクロールする度に発火させるかどうか
+    });
+});
