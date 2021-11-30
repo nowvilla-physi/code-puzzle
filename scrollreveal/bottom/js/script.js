f62b26d6b1f727f48627f7a9d66cec4241fd523e
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-11-30 22:03:33 +0900

    fix comment position.

--- scrollreveal/bottom/js/script.js
+++ scrollreveal/bottom/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         delay: 300,         // fadeInアニメーションを開始するまでの時間(ms)
         distance: '120px',  // スライドする要素の移動距離
         duration: 1000,     // fadeInアニメーションの時間(ms)
-        origin: 'bottom',      // どの方向からスライドするか
+        origin: 'bottom',   // どの方向からスライドするか
         reset: false        // fadeInアニメーションをスクロールする度に発火させるかどうか
     });
 });
