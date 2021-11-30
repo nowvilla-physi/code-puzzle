b59aa6a92ace1c33b89de5f2c5d617aa82ff0207
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-11-30 22:15:33 +0900

    add scroll reveal for horizontal rotation.

---
+++ scrollreveal/horizontalrotation/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    ScrollReveal().reveal('.image', {
+        delay: 300,      // fadeInアニメーションを開始するまでの時間(ms)
+        duration: 1000,  // fadeInアニメーションの時間(ms)
+        reset: false,    // fadeInアニメーションをスクロールする度に発火させるかどうか
+        rotate: {        // 回転の設定(3次元で指定可能)
+            x: 0,
+            y: 180,
+            z: 0
+        },
+    });
+});
