9e518e034507c6f6d8d067c189001b6a109f748e
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-12-12 11:16:22 +0900

    add slide show for auto play.

---
+++ slideshow/autoplay/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slide-container').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true
+    });
+});
