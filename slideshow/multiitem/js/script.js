b69c5146b18279bdda7c2adaa80bb570f7728233
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-12-12 11:00:53 +0900

    add slide show for multi items.

---
+++ slideshow/multiitem/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slide-container').slick({
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: true
+    });
+});
