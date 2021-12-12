fa809a1203feaf6ce8f11a42310d2b7013f4acc1
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-12-12 11:10:22 +0900

    add slide show for center.

---
+++ slideshow/center/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slide-container').slick({
+        dots: true,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+    });
+});
