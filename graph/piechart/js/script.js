7a0b77b645cda425a0d16bd2618230e94b98c2d7
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-12-18 14:58:42 +0900

    add graph for pie chart.

---
+++ graph/piechart/js/script.js
@@ -0,0 +1,34 @@
+const labels = [
+    'Red',
+    'Blue',
+    'Yellow'
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        data: [300, 50, 100],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+    }]
+};
+
+const config = {
+    type: 'pie',
+    data: data,
+};
+
+$(document).ready(function () {
+    showGraph();
+});
+
+function showGraph() {
+    const myChart = new Chart(
+        document.getElementById('canvas'),
+        config
+    );
+}
