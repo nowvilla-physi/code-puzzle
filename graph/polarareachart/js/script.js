a9db6f1d8d574edf63d46c3d4b554f5cd0d3334c
Tomoki-i <nowvilla.physi.311@gmail.com>
2021-12-18 15:03:12 +0900

    add graph for polar area chart.

---
+++ graph/polarareachart/js/script.js
@@ -0,0 +1,39 @@
+const labels = [
+    'Red',
+    'Green',
+    'Yellow',
+    'Grey',
+    'Blue'
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First Dataset',
+        data: [11, 16, 7, 3, 14],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(75, 192, 192)',
+            'rgb(255, 205, 86)',
+            'rgb(201, 203, 207)',
+            'rgb(54, 162, 235)'
+        ]
+    }]
+};
+
+const config = {
+    type: 'polarArea',
+    data: data,
+    options: {}
+};
+
+$(document).ready(function () {
+    showGraph();
+});
+
+function showGraph() {
+    const myChart = new Chart(
+        document.getElementById('canvas'),
+        config
+    );
+}
